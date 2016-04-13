04d47c80416ce7fbb67f7af96d3f86167c4197bc
mrnhuttrinh <mrnhuttrinh@hotmail.com>
2016-04-13 16:20:55 +0700

    Add chat into admin page

--- server/app/routes/authenticate.js
+++ server/app/routes/authenticate.js
@@ -1,15 +1,28 @@
+var jwt = require("jsonwebtoken");
+var config = require("../config");
+
 var models = require("../models");
 
 module.exports = {
     signin: function(req, res) {
-        models.User.findAll({
-            include: [ models.Task ]
-        }).then(function(users) {
-            res.send({
-                title: 'Express',
-                users: users
-            });
+        res.json({
+            token: jwt.sign({
+                "username": "user1"
+            }, config.secret)
         });
-        // res.send({"user": "user"})
+    },
+    me: function(req, res) {
+        // models.User.findAll({
+        //     include: [ models.Task ]
+        // }).then(function(users) {
+        //     res.send({
+        //         title: 'Express',
+        //         users: users
+        //     });
+        // });
+        res.send({
+            "user": "user",
+            "message": "login success"
+        })
     }
 };
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router  = express.Router();
+var authorized = require("../authorized");
 
 var authenticate = require("./authenticate");
 var menu = require("./menu");
 
-router.get('/', authenticate.signin);
+router.post('/signin', authenticate.signin);
+router.get('/me', authorized, authenticate.me);
+
 router.get("/menu", menu.get);
 
 module.exports = router;
