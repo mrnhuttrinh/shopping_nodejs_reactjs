a0531f312017302ac44f12275b3910f78c4fe79b
mrnhuttrinh <mrnhuttrinh@hotmail.com>
2016-04-17 00:16:30 +0700

    update config.json

--- client/admin/scripts/components/Widget.js
+++ client/admin/scripts/components/Widget.js
@@ -70,19 +70,28 @@ export default class Widget extends Component {
                 <div className="tab-content">
                     <div className="tab-pane active" id="home" role="tabpanel">
                         <div className="row">
-                            <div className="col-md-4">.col-md-4</div>
-                            <div className="col-md-4">.col-md-4</div>
-                            <div className="col-md-4">.col-md-4</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
                         </div>
                         <div className="row">
-                            <div className="col-md-4">.col-md-4</div>
-                            <div className="col-md-4">.col-md-4</div>
-                            <div className="col-md-4">.col-md-4</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
                         </div>
                         <div className="row">
-                            <div className="col-md-4">.col-md-4</div>
-                            <div className="col-md-4">.col-md-4</div>
-                            <div className="col-md-4">.col-md-4</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
+                            <div className="col-md-3 col-sm-6">.col-md-3</div>
                         </div>
                         <div className="row">
                             <div className="col-md-4"></div>
